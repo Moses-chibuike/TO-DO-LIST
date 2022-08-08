593fdbbf594928032842497f4ec4bd339af072d8
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2022-08-08 01:54:57 +0100

    almost done

--- main.js
+++ main.js
@@ -19,8 +19,9 @@ const list_el = document.querySelector("#tasks");
 
            const task_content_el = document.createElement("div");
            task_content_el.classList.add("content");
+        
            
-
+           window.localStorage.setItem('task', 'content');
            task_el.appendChild(task_content_el);
 
 
@@ -32,7 +33,42 @@ const list_el = document.querySelector("#tasks");
 
            task_content_el.appendChild(task_input_el);
 
+           const task_actions_el = document.createElement("div");
+           task_actions_el.classList.add("actions");
+
+           const task_edit_el = document.createElement("button");
+           task_edit_el.classList.add("edit");
+           task_edit_el.innerHTML ="Edit";
+           
+           const task_delete_el = document.createElement
+           ("button");
+           task_delete_el.classList.add("delete");
+           task_delete_el.innerHTML ="Delete";
+
+           task_actions_el.appendChild(task_edit_el);
+           task_actions_el.appendChild(task_delete_el);
+
+           task_el.appendChild(task_actions_el);
+
            list_el.appendChild(task_el);
+
+           input.value ="";
+
+           task_edit_el.addEventListener('click', () => {
+            if ( task_edit_el.innerText.toLowerCase() ==
+            "edit"){
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly","readonly");
+                task_edit_el.innerText = "Edit";
+            }
+           });
+
+           task_delete_el.addEventListener('click', () => {
+           list_el.removeChild(task_el);
+           });
            
      })
 })
